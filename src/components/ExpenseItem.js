cd3dc73d8415f83a7e659137ba78b86275e13b36
Zak <ream720@gmail.com>
2022-02-22 19:56:23 -0500

    ExpenseItem component HTML/js

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 20th 2020</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$29.26</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
