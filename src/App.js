8498241d8dbe82e8ac2e421657a44258f72be85a
Zak <ream720@gmail.com>
2022-02-25 11:40:45 -0500

    ExpenseForm and NewExpense for user input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from "react";
+
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -25,7 +28,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses}></Expenses>
     </div>
   );
