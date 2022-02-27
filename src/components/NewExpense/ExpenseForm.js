d58a47d1992f0b34999fbac9cd2859c082615c3e
Zak <ream720@gmail.com>
2022-02-27 14:03:06 -0500

    two-way binding form inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,26 +7,45 @@ const ExpenseForm = () => {
   const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (event) => {
-    console.log('title ' + event.target.value);
+    console.log("title " + event.target.value);
     setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    console.log('amount ' + event.target.value);
+    console.log("amount " + event.target.value);
     setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    console.log('date ' + event.target.value);
+    console.log("date " + event.target.value);
     setEnteredDate(event.target.value);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -34,6 +53,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -43,6 +63,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
