4d8e2954c92eb92121cd52fe3b8201891fd53b64
Zak <ream720@gmail.com>
2022-02-28 16:03:02 -0500

    Use State to track and update data on the page

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  //useState hook returns new variable called title in a 2-elemt array
-  //setTitle function changes value of title
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('UPDATED');
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
