cc7478bc953796882397d1bf4ca1027bfe68701d
Zak <ream720@gmail.com>
2022-02-28 19:48:31 -0500

    Chart and ChartBars wrap up

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,13 +4,19 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+    // takes dataPoints objects and maps just the value for each, returns array of values
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    //finding max value of expenses for each month
+    const totalMaximum = Math.max(...dataPointValues);
+
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -24,6 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         {/* renders based on expensesContent variable defined above */}
          <ExpensesList items={filteredExpenses} />
       </Card>
